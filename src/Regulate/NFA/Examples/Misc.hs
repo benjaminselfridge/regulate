7f8454d577ce69d75dc579ff073a5393382f3d85
Ben Selfridge <benselfridge@galois.com>
2022-03-30 16:55:32 -0700

    removes old modules

--- src/Regulate/NFA/Examples/Misc.hs
+++
@@ -1,9 +0,0 @@
-module Regulate.NFA.Examples.Misc where
-
-import Regulate.NFA
-
-csfs :: MNFA s Char
-csfs = symbol 'c' `cat` symbol 's' `cat` (star (symbol 'f' `cat` symbol 's'))
-
-sr :: MNFA s Char
-sr = star (symbol 's' `cat` symbol 'r')
